b446c3d1013104231bcc00768857721d0c6185f4
Marco Antonio Martin Callejo <marcomoguer@yahoo.es>
2013-02-25 05:46:45 +0100

    Ejemplo de uso de los proveedores de localización en Android para determinar la posición. Podemos utilizar el GPS y la red inhalámbrica como fuentes, cada una con una precisión y unas capacidades distintas. Por ejemplo el GPS proporciona la altura y es más preciso, pero tarda más tiempo en estar listo y consume más recursos que la localización por redes inhalámbricas, que son menos precisas pero más rápidas y con un menor consumo.

--- Posicionamiento/gen/com/example/posicionamiento/R.java
+++ Posicionamiento/gen/com/example/posicionamiento/R.java
@@ -24,16 +24,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080009;
-        public static final int bot_activar=0x7f080005;
-        public static final int bot_parar=0x7f080006;
-        public static final int etiq_altura=0x7f080004;
-        public static final int etiq_latitud=0x7f080001;
-        public static final int etiq_longitud=0x7f080002;
-        public static final int etiq_precision=0x7f080003;
-        public static final int etiq_proveedor=0x7f080008;
-        public static final int textView1=0x7f080007;
+        public static final int TextView1=0x7f080001;
+        public static final int action_settings=0x7f08000e;
+        public static final int bot_activar=0x7f080006;
+        public static final int bot_parar=0x7f080007;
+        public static final int btn_opciones=0x7f08000c;
+        public static final int etiq_altura=0x7f080005;
+        public static final int etiq_estado=0x7f080008;
+        public static final int etiq_hora=0x7f08000d;
+        public static final int etiq_latitud=0x7f080002;
+        public static final int etiq_longitud=0x7f080003;
+        public static final int etiq_precision=0x7f080004;
+        public static final int etiq_proveedor=0x7f080009;
+        public static final int main_posicionamiento=0x7f080000;
+        public static final int toggleGPS=0x7f08000a;
+        public static final int toggleNETWORK=0x7f08000b;
     }
     public static final class layout {
         public static final int posicionamiento_main=0x7f030000;
@@ -42,6 +47,11 @@ public final class R {
         public static final int posicionamiento_main=0x7f070000;
     }
     public static final class string {
+        public static final int GPS_OFF=0x7f050008;
+        public static final int GPS_ON=0x7f050006;
+        public static final int Network_OFF=0x7f050009;
+        public static final int Network_ON=0x7f050007;
+        public static final int Opciones_localizacion=0x7f05000a;
         public static final int acc_activar=0x7f050003;
         public static final int acc_parar=0x7f050004;
         public static final int action_settings=0x7f050001;
